81d879bcee8f823b8c28e383f86c35752f96cb0a
Mitchell <mitchellwong833@gmail.com>
2022-07-09 15:33:04 +1200

    Begun adding CRUD functionality to the API by enabling the handling of requests with the 'POST' method

--- pizza_joint/views.py
+++ pizza_joint/views.py
@@ -1,9 +1,21 @@
 from django.http import JsonResponse
 from .models import Pizza
 from .serializers import PizzaSeralizer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # method will retrive list of pizza data and return it in JSON 
+@api_view(['GET', 'POST'])
 def pizza_list(request):
-    pizzas = Pizza.objects.all() # get list as query set
-    serealizer = PizzaSeralizer(pizzas, many=True) # serialize
-    return JsonResponse(serealizer.data, safe=False) # convert serialized data to JSON
+
+    if request.method == 'GET':
+        pizzas = Pizza.objects.all() # get list as query set
+        serializer = PizzaSeralizer(pizzas, many=True) # serialize
+        return JsonResponse({"pizza": serializer.data}) # convert serialized data to JSON
+
+    if request.method == 'POST':
+        serializer = PizzaSeralizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
