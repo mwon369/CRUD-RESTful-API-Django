8cb638c95a166d405c3ec684b5a9798ef54fade5
Mitchell <mitchellwong833@gmail.com>
2022-07-09 14:23:36 +1200

    Added extra fields to our pizza model

--- pizza_joint/models.py
+++ pizza_joint/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class Pizza(models.Model):
+
+    SIZE_CHOICES = [
+        ("L", "Large"),
+        ("M", "Medium"),
+        ("S", "Small"),
+    ]
+
     name = models.CharField(max_length=100)
     ingredients = models.CharField(max_length=200)
     price = models.FloatField()
+    size = models.CharField(max_length=1, choices=SIZE_CHOICES, default="M")
+    isVegetarian = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
