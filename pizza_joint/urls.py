1643b3ce15c2b1795e221121a60915a6be8343c4
Mitchell <mitchellwong833@gmail.com>
2022-07-09 17:10:30 +1200

    Changed URL paths

--- pizza_joint/urls.py
+++ pizza_joint/urls.py
@@ -20,8 +20,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pizza_joint/', views.pizza_list),
-    path('pizza_joint/<int:id>', views.pizza_detail)
+    path('pizza/', views.pizza_list),
+    path('pizza/<int:id>', views.pizza_detail)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
