b34c1518c78e795948d71a4efded993823f0c979
Mitchell <mitchellwong833@gmail.com>
2022-07-09 13:56:15 +1200

    Created model and migration for that model

---
+++ pizza_joint/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.CharField(max_length=200)
+    price = models.FloatField()
--- pizza_joint/settings.py
+++ pizza_joint/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pizza_joint',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
